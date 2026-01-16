aafcbda077b84c4c23f300f3b79ce7bf98b17e39
aliffianilhamf <aliffianilhamf@gmail.com>
2026-01-16 20:43:59 +0700

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://bikin-software.vercel.app",
   integrations: [mdx(), sitemap()],
 
   vite: {
