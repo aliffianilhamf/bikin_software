aa19c4ec16b3d575a99967ce5c9cf79a7b942436
aliffianilhamf <aliffianilhamf@gmail.com>
2026-01-16 20:41:04 +0700

    Add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,25 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
+  },
+  i18n: {
+    defaultLocale: "id",
+    locales: ["id", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
   },
 });
